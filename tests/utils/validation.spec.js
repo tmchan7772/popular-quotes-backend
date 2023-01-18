509c28c80e29dfa4b251644d98a8caa97f4a1ea1
Misha Chan <misha.chan@americanwell.com>
2023-01-18 19:28:30 +0300

    Configuring mocha + test for validation

---
+++ tests/utils/validation.spec.js
@@ -0,0 +1,38 @@
+import assert from 'assert';
+import validateRequiredFields from '../../src/utils/validation.js';
+
+describe('Utils.validation', () => {
+  it('validateRequiredFields should return empty array', () => {
+    const obj = { propA: 123 };
+
+    const result = validateRequiredFields(obj, ['propA']);
+
+    assert.equal(result.length, 0);
+  });
+
+  it('validateRequiredFields should return array for undefined props', () => {
+    const obj = { propA: undefined };
+    const expectedResult = ['propA', 'propB'];
+
+    const result = validateRequiredFields(obj, ['propA', 'propB']);
+
+    assert.deepEqual(result, expectedResult);
+  });
+
+  it('validateRequiredFields should return array for null props', () => {
+    const obj = { propA: null };
+    const expectedResult = ['propA'];
+
+    const result = validateRequiredFields(obj, ['propA']);
+
+    assert.deepEqual(result, expectedResult);
+  });
+
+  it('validateRequiredFields should not return array for prop === 0', () => {
+    const obj = { propA: 0 };
+
+    const result = validateRequiredFields(obj, ['propA']);
+
+    assert.equal(result.length, 0);
+  });
+});
